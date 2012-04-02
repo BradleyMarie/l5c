9b7d768d4fd286f6f20a2b5c7c1dd062ffe67e28
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2012-04-01 19:38:53 -0500

    labels now padded by parser

--- l1c/l1c.rkt
+++ l1c/l1c.rkt
@@ -2,8 +2,12 @@
 
 (print-only-errors #t)
 
-(define (label? expr)
-  (symbol? expr))
+(define (l1c-label? expr)
+  (if (symbol? expr)
+      (match (symbol->string expr)
+        [(regexp #rx"^[a-zA-Z_][a-zA-Z_0-9]*$") #t]
+        [_ #f])
+      #f))
 
 (define (num? expr)
   (number? expr))
@@ -34,11 +38,20 @@
   (or (num? expr) (x? expr)))
 
 (define (s? expr)
-  (or (x? expr) (num? expr) (label? expr)))
+  (or (x? expr) (num? expr) (l1c-label? expr)))
 
 (define (u? expr)
-  (or (x? expr) (label? expr)))
+  (or (x? expr) (l1c-label? expr)))
 
+(define (parse-l1-label label)
+  (string->symbol (string-append "l1c_" (substring (symbol->string label) 1))))
+
+(define (l1-label? expr)
+  (if (symbol? expr)
+      (match (symbol->string expr)
+        [(regexp #rx"^:[a-zA-Z_][a-zA-Z_0-9]*$") #t]
+        [_ #f])
+      #f))
 
 (define-type L1Instruction
   [assign-register (dest x?) (source s?)]
@@ -48,9 +61,9 @@
   [update-sop-sx (lhs x?) (operation sop?) (rhs sx?)]
   [update-sop-num (lhs x?) (operation sop?) (rhs num?)]
   [save-comparison (dest cx?) (lhs t?) (operation cmp?) (rhs t?)]
-  [l1-label (name label?)]
-  [goto-label (label label?)]
-  [cond-jump (cond-lhs t?) (cond-operation cmp?) (cond-rhs t?) (true-label label?) (false-label label?)]
+  [label (name l1c-label?)]
+  [goto-label (label l1c-label?)]
+  [cond-jump (cond-lhs t?) (cond-operation cmp?) (cond-rhs t?) (true-label l1c-label?) (false-label l1c-label?)]
   [call-func (func-ref u?)]
   [tail-call-func (func-ref u?)]
   [return-from-func]
@@ -60,35 +73,77 @@
 
 (define (parse-instruction sexpr)
   (match sexpr
+    ; (x <- (mem x n4)) ; read from memory @ x+n4
     [(list dest '<- (list 'mem source-base source-offset ) )
      (read-memory dest source-base source-offset)]
+    
+    ; ((mem x n4) <- s) ;; update memory @ x+n4
+    [(list (list 'mem dest-base dest-offset) '<- (? l1-label? source))
+     (update-memory dest-base dest-offset (parse-l1-label source))]
     [(list (list 'mem dest-base dest-offset) '<- source)
      (update-memory dest-base dest-offset source)]
-    [(list dest '<- source) (assign-register dest source)]
+    
+    ; (x <- s) ;; assign to a register
+    [(list dest '<- (? l1-label? source))
+     (assign-register dest (parse-l1-label source))]
+    [(list dest '<- source)
+     (assign-register dest source)]
+    
+    ; (x aop= t) ;; update x with an arith op and t.
     [(list lhs (? aop? aop) rhs)
      (update-aop lhs aop rhs)]
+    
+    ; (x sop= sx) ;; update x with a shifting op and sx.
     [(list lhs (? sop? sop) (? sx? rhs))
      (update-sop-sx lhs sop rhs)]
+    
+    ; (x sop= num) ;; update x with a shifting op and num.
     [(list lhs (? sop? sop) (? num? rhs))
      (update-sop-num lhs sop rhs)]
+    
+    ; (cx <- t cmp t) ;; save result of a comparison
     [(list dest '<- lhs cmp rhs)
      (save-comparison dest lhs cmp rhs)]
+    
+    ; (goto label) ;; unconditional jump
     [(list 'goto label)
-     (goto-label label)]
+     (goto-label (parse-l1-label label))]
+    
+    ; (cjump t cmp t label label) ;; conditional jump
     [(list 'cjump cond-lhs cond-operation cond-rhs true-label false-label)
-     (cond-jump cond-lhs cond-operation cond-rhs true-label false-label)]
+     (cond-jump cond-lhs cond-operation cond-rhs (parse-l1-label true-label) (parse-l1-label false-label))]
+    
+    ; (call u) ;; call a function
+    [(list 'call (? l1-label? func-ref))
+     (call-func (parse-l1-label func-ref))]
     [(list 'call func-ref)
      (call-func func-ref)]
+    
+    ; (tail-call u) ;; tail call a function
+    [(list 'tail-call (? l1-label? func-ref))
+     (tail-call-func (parse-l1-label func-ref))]
     [(list 'tail-call func-ref)
      (tail-call-func func-ref)]
+    
+    ; (return) ;; return from a function 
     [(list 'return)
      (return-from-func)]
+    
+    ; (eax <- (print t))
     [(list 'eax '<- (list 'print source))
      (print-t source)]
+    
+    ; (eax <- (allocate t t))
     [(list 'eax '<- (list 'allocate size num))
      (allocate-t size num)]
+    
+    ; (eax <- (array-error t t))
     [(list 'eax '<- (list 'array-error base index))
-     (array-error-t base index)]))
+     (array-error-t base index)]
+    
+    ; label ;; target of a jump
+    [(? l1-label? l)
+     (label (parse-l1-label l))]))
 
 (test (parse-instruction '(eax <- ebx)) (assign-register 'eax 'ebx))
 (test (parse-instruction '(eax <- 3)) (assign-register 'eax 3))
