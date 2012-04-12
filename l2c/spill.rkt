c5fe54f87283d609d3914f80a18db11e66fb51b3
Brad Weinberger <brad@romulus.planets>
2012-04-12 00:47:36 -0500

    Removed print statements

--- l2c/spill.rkt
+++ l2c/spill.rkt
@@ -17,74 +17,52 @@
 ;; Helper Functions
 ;;
 
-(define is-last-instruction #f)
-
-(define (print-line string)
-  (begin
-    (display string)
-    (if (not is-last-instruction) (newline) (void))))
-
-(define (replace-list-elements element find-value replace-value)
+(define (replace-list-elements list-of-symbols find-value replace-value)
   (cond
-    [(eq? element find-value) replace-value]
-    [(list? element) (map (lambda (e) (replace-list-elements e find-value replace-value)) element)]
-    [else element]))
-
-(define (print-modified-list find-value replace-value original-list)
-  (print-line (map (lambda (element) (replace-list-elements element find-value replace-value)) original-list)))
+    [(eq? list-of-symbols find-value) replace-value]
+    [(list? list-of-symbols) (map (lambda (e) (replace-list-elements e find-value replace-value)) list-of-symbols)]
+    [else list-of-symbols]))
 
 (define (read-memory variable offset)
-  (printf "(~a <- (mem ebp ~a))\n" variable offset ))
+  (list variable '<- (list 'mem 'ebp offset)))
 
 (define (write-memory variable offset)
-  (printf "((mem ebp ~a) <- ~a)\n" offset variable ))
+  (list (list 'mem 'ebp offset) '<- variable))
 
 (define prefix-index -1)  ;; Number
 (define prefix (void))       ;; String
-(define offset (void))      ;; String
+(define offset (void))      ;; Number
 (define old-variable (void))  ;; Symbol
 
 (define (spill-variable)
   (begin
     (set! prefix-index (+ prefix-index 1))
-    (string-append prefix (number->string prefix-index))))
+    (string->symbol (string-append prefix (number->string prefix-index)))))
 
 (define (spill-read original-expression)
   (let ([new-variable (spill-variable)])
-    (begin
+    (list
       (read-memory new-variable offset)
-      (print-modified-list old-variable new-variable original-expression))))
+      (replace-list-elements original-expression old-variable new-variable))))
 
 (define (spill-write original-expression)
   (let ([new-variable (spill-variable)])
-    (begin
-      (print-modified-list old-variable new-variable original-expression)
+    (list
+      (replace-list-elements original-expression old-variable new-variable)
       (write-memory new-variable offset))))
 
 (define (spill-read-write original-expression)
   (let ([new-variable (spill-variable)])
-    (begin
+    (list
       (read-memory new-variable offset)
-      (print-modified-list old-variable new-variable original-expression)
+      (replace-list-elements original-expression old-variable new-variable)
       (write-memory new-variable offset))))
-
-(define (spill-from-memory x n4)
-  (let ([new-variable (spill-variable)])
-    (begin
-      (printf "(~a <- (mem ~a ~a))\n" new-variable x n4)
-      (write-memory new-variable offset))))
-
-(define (spill-to-memory x n4)
-  (let ([new-variable (spill-variable)])
-    (begin
-      (read-memory new-variable offset)
-      (printf "((mem ~a ~a) <- ~a)\n" x n4 new-variable))))
 
 (define (write-to-x dest)
-  (printf "(~a <- (mem ebp ~a))\n" dest offset))
+  (list (list dest '<- (list 'mem 'ebp offset))))
 
 (define (read-from-s source)
-  (printf "((mem ebp ~a) <- ~a)\n" offset source))
+  (list (list (list 'mem 'ebp offset) '<- source)))
 
 ;;
 ;; Parser
@@ -93,48 +71,54 @@
 (define (spill-instruction sexpr)
   (match sexpr
     ; (eax <- (print t))
-    [(list 'eax '<- (list 'print source))
-     (if (eq? source old-variable) (spill-read sexpr) (print-line sexpr))]
+    [`(eax <- (print ,source))
+     (if (eq? source old-variable) (spill-read sexpr) sexpr)]
     
     ; (eax <- (allocate t t))
     ; (eax <- (array-error t t))
-    [(or (list 'eax '<- (list 'allocate v1 v2))
-         (list 'eax '<- (list 'array-error v1 v2)))
+    [(or `(eax <- allocate ,v1 ,v2)
+         `(eax <- array-error ,v1 ,v2))
      (if (or (eq? v1 old-variable) (eq? v2 old-variable))
-         (spill-read sexpr) (print-line sexpr))]
+         (spill-read sexpr) (list sexpr))]
     
     ; (x <- (mem x n4)) ; read from memory @ x+n4
-    [(list dest '<- (list 'mem source-base source-offset ))
-     (spill-from-memory source-base source-offset)]
+    [`(,dest <- (mem ,source-base ,source-offset))
+     (cond
+       [(and (eq? dest old-variable) (eq? source-base old-variable))
+        (spill-read-write sexpr)]
+       [(eq? dest old-variable) (spill-write sexpr)]
+       [(eq? source-base old-variable) (spill-read sexpr)]
+       [else (list sexpr)])]
     
     ; ((mem x n4) <- s) ;; update memory @ x+n4
-    [(list (list 'mem dest-base dest-offset) '<- source) 
-     (spill-to-memory dest-base dest-offset)]
+    [`((mem ,dest-base ,dest-offset) <- ,source)
+     (if (or (eq? dest-base old-variable) (eq? source old-variable))
+         (spill-read sexpr) (list sexpr))]
     
     ; (x <- s) ;; assign to a register
-    [(list dest '<- source)
+    [`(,dest <- ,source)
      (cond
        [(and (eq? source old-variable) (eq? dest old-variable)) (void)]
        [(eq? source old-variable) (write-to-x dest)]
        [(eq? dest old-variable) (read-from-s source)]
-       [else (print-line sexpr)])]
+       [else (list sexpr)])]
     
     ; (x aop= t) ;; update x with an arith op and t.
     ; (x sop= sx) ;; update x with a shifting op and sx.
-    [(list lhs (? op? op) rhs)
+    [`(,lhs ,(? op? op) ,rhs)
      (cond
        [(eq? lhs old-variable) (spill-read-write sexpr)]
        [(eq? rhs old-variable) (spill-read sexpr)]
-       [else (print-line sexpr)])]
+       [else (list sexpr)])]
     
     ; (goto label) ;; unconditional jump
     ; (call u) ;; call a function
     ; (tail-call u) ;; tail call a function
-    [(or (list 'goto label) (list 'call label) (list 'tail-call label))
-     (if (eq? label old-variable) (spill-read sexpr) (print-line sexpr))]
+    [(or `(goto ,label) `(call ,label) `(tail-call ,label))
+     (if (eq? label old-variable) (spill-read sexpr) (list sexpr))]
     
-    ; (cx <- t cmp t) ;; save result of a comparison
-    [(list dest '<- lhs (? cmp? cmp) rhs)
+    ; (cx <- t cmp t) ;; save result of a comparison;
+    [`(,dest <- ,lhs ,(? cmp? cmp) ,rhs)
      (cond
        [(and (eq? dest old-variable) (or (eq? lhs old-variable) (eq? rhs old-variable)))
         (spill-read-write sexpr)]
@@ -142,27 +126,14 @@
         (spill-read sexpr)]
        [(eq? dest old-variable)
         (spill-write sexpr)]
-       [else (print-line sexpr)])]
+       [else (list sexpr)])]
     
     ; (cjump t cmp t label label) ;; conditional jump
-    [(list 'cjump lhs (? cmp? cmp) rhs true-label false-label)
+    [`(cjump ,lhs ,(? cmp? cmp) ,rhs ,true-label ,false-label)
      (if (or (eq? lhs old-variable) (eq? rhs old-variable))
-         (spill-read sexpr) (print-line sexpr))]
+         (spill-read sexpr) (list sexpr))]
     
-    [_ (print-line sexpr)]))
-
-(define (spill-function function)
-  (begin
-    (printf "(")
-    (map (lambda (instruction)
-           (begin
-             (if (eq? instruction (last function))
-                 (set! is-last-instruction #t)
-                 (void))
-             (spill-instruction instruction)))
-         function)
-    (printf ")")
-    (void)))
+    [_ (list sexpr)]))
 
 ;;
 ;; Compile the file specified on the command line
@@ -180,4 +151,4 @@
         (set! old-variable (read p))
         (set! offset (number->string (read p)))
         (set! prefix (symbol->string (read p)))
-        (spill-function sexpr)))))
+        (display (append* (map spill-instruction sexpr)))))))
